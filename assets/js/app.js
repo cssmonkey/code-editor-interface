28cfbbc56117bd807dfa59965a42d5888beb9482
Sean Jones <web@sean-jones.co.uk>
2015-12-02 14:49:37 +0000

    layout toggle, inline view

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,18 @@
 window.APP = (function (module, $) {
     "use strict";
 
+    var resizeTimerId;
+    // global(ish) variable for the current width of the device
+    APP.deviceWidth = $(window).resize(function () {
+        clearTimeout(resizeTimerId);
+        APP.deviceWidth = $(this).width();
+        resizeTimerId = setTimeout(function () {
+            $(window).trigger("onResizeEnd");
+        }, 250);
+    }).width();
+
     $(function() {
-      module.draggablePanels.init();
+      module.draggablePanels();
     });
 
   return module;
--- assets/js/modules/module.draggablePanels.js
+++ assets/js/modules/module.draggablePanels.js
@@ -3,78 +3,153 @@ window.APP = (function (module, $) {
 
   module.draggablePanels = {};
 
-  function DraggablePanels() {
+  var panels = {},
+      $window = $(window);
+
+  function TopPanels($container) {
     var self = this,
-        $container = $('.resizable');
+        $panelsContainer = $container,
+        $resizerHandle = $('.resizer-controls__handle', $panelsContainer);
 
-    var setUpInlineLayout = function(_$container) {
-      var $dragX = $('.resizable__resizer--xaxis', _$container);
+    self.init = function() {
+      console.log('top hiya');
+    };
+  };
 
-      $dragX.draggabilly({axis: 'x', containment: _$container});
+  function SidePanels($container) {
+    var self = this,
+        $panelsContainer = $container,
+        $resizerHandle = $('.resizer-controls__handle', $panelsContainer);
 
-      $dragX.on('dragMove', function() {
-        var dragData = $(this).data('draggabilly'),
-            indx = $(this).is(':first-child') ? 0 : 2,
-            $panel = $('.resizable-panel', _$container).eq(indx),
-            dragAmount = indx == 2 ? _$container.width() - dragData.position.x : dragData.position.x,
-            $otherResizer = $(this).siblings(),
-            $otherPanels = $('.resizable-panel', _$container).filter(function(i){return i != indx;}),
-            offsetAmount = $otherPanels.eq(0).position().left + $otherPanels.eq(0).outerWidth();
+    self.init = function() {
+      console.log('side hiya');
+    };
+  };
 
-          console.log(dragAmount);
+  function InlinePanels($container) {
+    var self = this,
+        $panelsContainer = $container,
+        $resizerHandle = $('.resizer-controls__handle', $panelsContainer),
+        options = {
+          axis: 'x',
+          containment: $container
+        };
 
-          $panel.css('flex', '0 0 '+ dragAmount + 'px');
-          $otherResizer.css('left', offsetAmount);
+    var onDrag = function() {
+      var data = $(this).data('draggabilly'),
+          indx = $(this).is(':first-child') ? 0 : 2,
+          $panel = $('.resizable-panel', $panelsContainer).eq(indx),
+          dragAmount = indx == 2 ? $panelsContainer.width() - data.position.x : data.position.x,
+          $otherResizer = $(this).siblings(),
+          $otherPanels = $('.resizable-panel', $panelsContainer).filter(function(i){return i != indx;}),
+          offsetAmount = $otherPanels.eq(0).position().left + $otherPanels.eq(0).outerWidth();
 
-          // if(!$otherPanels.hasClass('resizable-panel--rigid')) {
-          //   $otherPanels.each(function() {
-          //     var thisWidth = $(this).width();
-          //     $(this).css('flex', thisWidth +'px 0 0');
-          //   })
-          //   $otherPanels.addClass('resizable-panel--rigid')
-          // }
-
-      });
-      $dragX.on('dragEnd', function() {
-        $('.resizable-panel--rigid', _$container).removeClass('resizable-panel--rigid');
-      });
-      
-      _$container.on('click', '.resizable__fullsize', function(e) {
-        e.preventDefault();
-        
-        var $container = $(this).closest('.resizable'),
-            $thisPanel = $(this).closest('.resizable-panel'),
-            $otherPanels = $('.resizable-panel', $container).filter(function(i){return i != $thisPanel.index();});
-        
-        $otherPanels.css('flex', '0 0 0');
-        $thisPanel.css('flex', '1 0 0');
-        console.log($container);
-        $container.addClass('resizable--fullsize-enabled');
-      });
-
+      $otherPanels.removeAttr('style');
+      $panel.css('flex', '0 0 '+ dragAmount + 'px');
+      $otherResizer.css('left', offsetAmount);
     }
-    this.init = function() {
-      $container.each(function(i) {
-        var layoutType = $(this).data('layout');
-
-        switch(layoutType) {
-          case 'inline':
-            setUpInlineLayout($(this));
-            break;
-          case 'top':
-            break;
-          case 'side':
-            break;
-        }
-      });
-//var draggableBars = $elem.draggabilly({axis: 'x', containment: $container});
+    self.init = function() {
+        // setup dragging
+        $resizerHandle.draggabilly(options);
+        $resizerHandle.on('dragMove', onDrag);
+        $resizerHandle.on('dragStart', function(e, pointer) {
+          $('.resizer-controls__handle--active', $panelsContainer).removeClass('resizer-controls__handle--active');
+          $(this).addClass('resizer-controls__handle--active');
+        });
     }
-    this.addNew = function($container) {
+  }
+  // for when 2 panels are displayed on top, 3rd panel sits below
+  panels.top = function($container) {
+    var panels = new TopPanels($container);
+    panels.init();
+  };
+  // for panels arranged alongside each other
+  panels.inline = function($container) {
+    var panels = new InlinePanels($container);
+    panels.init();
+  };
+  // for when 2 panels are displayed on lefthand side, 3rd panel sits to the right
+  panels.side = function($container) {
+    var panels = new SidePanels($container);
+    panels.init();
+  };
+  panels.expandCollapse = function() {
+    var $expandBtn = $('.resizer-btn--expand'),
+        $collapseBtn = $('.resizer-btn--collapse');
+
+    $expandBtn.on('click', function(e) {
+      e.preventDefault();
+      var $panelsContainer = $(this).closest('.resizable'),
+          $thisPanel = $(this).closest('.resizable-panel');
+
+      $thisPanel.removeAttr('style');
+
+      if($thisPanel.hasClass('resizable-panel--expand')) {
+        $thisPanel.removeClass('resizable-panel--expand');
+      }
+      else {
+        $thisPanel.addClass('resizable-panel--expand');
+      }
+      adjustResizerPosition($panelsContainer);
+    });
+    $collapseBtn.on('click', function(e) {
+      e.preventDefault();
+      var $panelsContainer = $(this).closest('.resizable'),
+          $thisPanel = $(this).closest('.resizable-panel');
+
+      $thisPanel.removeAttr('style');
+
+      if($thisPanel.hasClass('resizable-panel--collapse')) {
+        $thisPanel.removeClass('resizable-panel--collapse');
+      }
+      else {
+        $thisPanel.addClass('resizable-panel--collapse');
+      }
+      adjustResizerPosition($panelsContainer);
+    });
+
+    function adjustResizerPosition($panelsContainer) {
 
     }
   }
+  panels.toggleLayout = function($container) {
+    var $layoutToggle = $('.resizer-controls--layout-toggle .btn', $container),
+        btnSelectedClass = 'btn--active';
 
-  module.draggablePanels = new DraggablePanels;
+    $layoutToggle.on('click', function(e) {
+      e.preventDefault();
+
+      if(!$(this).hasClass(btnSelectedClass)) {
+        var selectedLayout = $(this).data('layouttoggle');
+
+        $('.' + btnSelectedClass, $container).removeClass(btnSelectedClass);
+        $(this).addClass(btnSelectedClass);
+
+        $container.attr('data-layout', selectedLayout);
+        $('.resizable-panel', $container).removeAttr('style');
+        $('.resizer-controls__handle', $container).removeAttr('style');
+        panels[selectedLayout]($container);
+      }
+
+    });
+  }
+  var init = function() {
+    var $panelsContainer = $('.resizable');
+
+    panels.expandCollapse();
+    $panelsContainer.each(function(i) {
+      var layoutType = $(this).data('layout');
+      panels[layoutType]($(this));
+      panels.toggleLayout($(this));
+    });
+
+    $window.on('onResizeEnd', function() {
+      $('.resizer-controls__handle').removeAttr('style');
+      $('.resizable-panel').removeAttr('style');
+    });
+  }
+
+  module.draggablePanels = function(){init()};
 
   return module;
 })(window.APP || {}, window.jQuery);
